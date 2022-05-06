3f38ed3583ba3b8bd18b6527c006a1c7ee2852ce
Chris Hamill <chamill@ets.org>
2022-05-06 14:26:19 -0400

    add image filetype to defaults

--- src/utils/defaults.py
+++ src/utils/defaults.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 DEFAULTS = {
     'IMG_DIR' : Path(__file__).resolve().parent.parent.parent / 'img',
-    'SAVE' : {
+    'IMG_FTYPE' : '.png',
+   'SAVEFIG-KWARGS' : {
         'bbox_inches' : 'tight',
         'dpi' : 150,
         'figsize' : (10, 15)
--- src/utils/parser.py
+++ src/utils/parser.py
@@ -9,13 +9,17 @@ class Parser:
             description=f'{" Go make some art, nerd !!! ":*^100}'
         )
         self.parser.add_argument(
-            '-s', '--save_image',
-            required=False,
+            '-S', '--save_image',
             nargs='*',
             help='Pass to save image, else image will only be displayed via a '
                  'popup window. If passing a filepath, that will be used to '
                  'save the image, otherwise the following path will be used: '
                  f'{DEFAULTS["IMG_DIR"]}/<script_basename>.png'
+        )
+        self.parser.add_argument(
+            '-E', '--filetype_extension',
+            default=DEFAULTS['IMG_FTYPE'],
+            help='Filetype extension to use when saving images.'
         )
 
     def add(self, *args, **kwargs):
