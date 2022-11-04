e1c6176a38c753504bc7324715ba42aabdf1c103
Chris Hamill <chamill@ets.org>
2022-11-04 10:14:34 -0400

    improve setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,27 @@ import nerdart
 from setuptools import find_packages, setup
 
 
+# package metadata
+NAME = 'nerdart'
+VERSION = nerdart.__version__
+DESCRIPTION = 'Nerdy art made with math and code.'
+URL = 'https://github.com/cohml/nerdart'
+AUTHOR = 'cohml'
+REQUIRES_PYTHON = '>=3.9.0'
+ENTRY_POINTS = [
+    'nerdart = nerdart.utils.run:main'
+]
+
+
 setup(
-    name='nerdart',
-    version=nerdart.__version__,
-    description='Nerdy art made with math and code.',
-    url='https://github.com/cohml/nerdart',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    entry_points={'console_scripts' : ENTRY_POINTS},
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'console_scripts' : [
-            'nerdart = nerdart.utils.run:main'
-        ]
-    }
+    zip_safe=False
 )
