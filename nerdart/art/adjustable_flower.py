dd732f058eb968811314963aecc84f397ab1b48f
Chris Hamill <chamill@ets.org>
2022-11-04 13:01:18 -0400

    add size param to adjustible_flower

--- nerdart/art/adjustable_flower.py
+++ nerdart/art/adjustable_flower.py
@@ -10,13 +10,14 @@ def plot(args):
     bleep = args.bleep
     bloop = args.bloop
     blap = args.blap
+    size = args.size
 
     coords = np.linspace(np.pi * bleep, np.pi * bloop, blap)
     x, y = xy(coords)
     colors = plt.cm.prism(np.linspace(0, 1, blap))
 
     ax = plt.subplot()
-    ax.scatter(x * (x + coords), y * (y + coords), c=colors, alpha=0.75)
+    ax.scatter(x * (x + coords), y * (y + coords), c=colors, alpha=0.75, s=size)
     ax.set_aspect('equal')
     ax.axis('off')
 
@@ -26,6 +27,7 @@ def main():
     parser.add('-e', '--bleep', type=int, default=-100)
     parser.add('-o', '--bloop', type=int, default=100)
     parser.add('-a', '--blap', type=int, default=1000)
+    parser.add('-s', '--size', type=int, default=20)
     args = parser.parse()
     plot(args)
 
