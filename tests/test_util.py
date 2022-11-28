111924a9e71a4c2d899f386623970e69c2416dd4
Chris Hamill <chamill@ets.org>
2022-11-28 11:04:52 -0500

    Make test output more informative

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,9 +5,11 @@ from nerdart.utils import get_artwork_paths, xy
 
 def test_get_artwork_paths():
     computed_artwork_paths = get_artwork_paths()
-    assert len(computed_artwork_paths) == 45
-    assert computed_artwork_paths[0].stem == "unnamed"
-    assert computed_artwork_paths[-1].stem == "smoke"
+    error_msg = "Have you committed all new artworks?"
+    assert len(computed_artwork_paths) == 45, error_msg
+    error_msg += " This test relies on the alphabetical order of artwork names."
+    assert computed_artwork_paths[0].stem == "unnamed", error_msg
+    assert computed_artwork_paths[-1].stem == "smoke", error_msg
 
 
 def test_xy(array):
